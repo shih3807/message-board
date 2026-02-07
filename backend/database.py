4740a43b2f15ec2da24088162ef52048f5084ebb
shih <Poetic0124@icloud.com>
2026-02-07 13:50:26 +0800

    Feat: Add PostgreSQL and update database connection

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:" \
+               f"{os.getenv('POSTGRES_PASSWORD')}@" \
+               f"{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/" \
+               f"{os.getenv('POSTGRES_DB')}"
 
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(DATABASE_URL, pool_size=5,
+    max_overflow=10)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
